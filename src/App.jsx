41279795e861656ec7531614d3a5225fe41af2f2
RihanC <crihan082@gmail.com>
2026-04-14 04:18:12 -0700

    completed employee dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboards/EmployeeDashboard'
+import AdminDashboard from './components/Dashboards/AdminDashboard'
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
-      <EmployeeDashboard />
+      {/* <EmployeeDashboard /> */}
+      <AdminDashboard />
     
     </>
       
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,13 +2,69 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className='h-[65%] w-full flex justify-start bg-fuchsia-300 mt-15 rounded-xl p-5 gap-5'>
-        <div className='h-full w-150 bg-yellow-300 rounded-xl'>
-
+    <div id='tasklist' className=' h-[65%] overflow-x-auto w-full flex justify-start mt-15 rounded-xl p-5 gap-5'>
+        <div className='shrink-0 h-full w-150 bg-yellow-300 rounded-xl p-5 '>
+          <div className='flex justify-between'>
+            <h3 className='bg-red-400 px-4 py-2 rounded-xl'> High</h3>
+            <h4 className='text-sm'>24 Oct 2025</h4>
+          </div>
+          <h2 className='text-2xl font-semibold'>Do Assignment-6 of DBMS.</h2>
+          <p className='text-sm mt-4 p-4'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta est ex voluptate harum placeat. Exercitationem!
+          </p>
         </div>
-        <div className='h-full w-150 bg-yellow-300 rounded-xl'>
-
+        <div className='shrink-0 h-full w-150 bg-green-300 rounded-xl p-5 '>
+          <div className='flex justify-between'>
+            <h3 className='bg-red-400 px-4 py-2 rounded-xl'> High</h3>
+            <h4 className='text-sm'>24 Oct 2025</h4>
+          </div>
+          <h2 className='text-2xl font-semibold'>Do Assignment-6 of DBMS.</h2>
+          <p className='text-sm mt-4 p-4'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta est ex voluptate harum placeat. Exercitationem!
+          </p>
         </div>
+        <div className='shrink-0 h-full w-150 bg-blue-300 rounded-xl p-5 '>
+          <div className='flex justify-between'>
+            <h3 className='bg-red-400 px-4 py-2 rounded-xl'> High</h3>
+            <h4 className='text-sm'>24 Oct 2025</h4>
+          </div>
+          <h2 className='text-2xl font-semibold'>Do Assignment-6 of DBMS.</h2>
+          <p className='text-sm mt-4 p-4'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta est ex voluptate harum placeat. Exercitationem!
+          </p>
+        </div>
+        <div className='shrink-0 h-full w-150 bg-emerald-300 rounded-xl p-5 '>
+          <div className='flex justify-between'>
+            <h3 className='bg-red-400 px-4 py-2 rounded-xl'> High</h3>
+            <h4 className='text-sm'>24 Oct 2025</h4>
+          </div>
+          <h2 className='text-2xl font-semibold'>Do Assignment-6 of DBMS.</h2>
+          <p className='text-sm mt-4 p-4'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta est ex voluptate harum placeat. Exercitationem!
+          </p>
+        </div>
+        <div className='shrink-0 h-full w-150 bg-yellow-300 rounded-xl p-5 '>
+          <div className='flex justify-between'>
+            <h3 className='bg-red-400 px-4 py-2 rounded-xl'> High</h3>
+            <h4 className='text-sm'>24 Oct 2025</h4>
+          </div>
+          <h2 className='text-2xl font-semibold'>Do Assignment-6 of DBMS.</h2>
+          <p className='text-sm mt-4 p-4'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta est ex voluptate harum placeat. Exercitationem!
+          </p>
+        </div>
+        <div className='shrink-0 h-full w-150 bg-red-300 rounded-xl p-5 '>
+          <div className='flex justify-between'>
+            <h3 className='bg-red-400 px-4 py-2 rounded-xl'> High</h3>
+            <h4 className='text-sm'>24 Oct 2025</h4>
+          </div>
+          <h2 className='text-2xl font-semibold'>Do Assignment-6 of DBMS.</h2>
+          <p className='text-sm mt-4 p-4'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta est ex voluptate harum placeat. Exercitationem!
+          </p>
+        </div>
+
+
     
     </div>
         
